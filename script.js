71ea338a017a6e80978d3d2deb8d50ef32bd3af0
thospcos <bukott25@gmail.com>
2025-12-10 20:37:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const webhookURL = "https://discord.com/api/webhooks/1444318939751518369/-9blXMbgbRx-r-Frr6OgENLAhgB_H3Vg6LV37u6qejKaFRcjSKOgqd5l5TYaHM_QQzGr";
+
+document.getElementById("contactForm").addEventListener("submit", e => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  fetch(webhookURL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      content:
+        "**New thOSp Form Submission (Send a message to us!)**\n" +
+        `**Name:** ${name}\n` +
+        `**Email:** ${email}\n` +
+        `**Message:** ${message}`
+    })
+  }).then(() => {
+    alert("Message sent 🎵");
+    e.target.reset();
+  }).catch(() => alert("Failed to send ❌"));
+});
